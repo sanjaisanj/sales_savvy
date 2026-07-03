669953d9c949278abe2180e1f18cc4c1771f6478
S Sanjai <sanjaisanjai10449@gmail.com>
2026-07-03 10:02:45 +0530

    Use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './Routes';
-import './assets/styles.css';
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import AppRoutes from "./Routes";
+import "./assets/styles.css";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <AppRoutes />
-    </Router>
+    </HashRouter>
   );
 }
 
